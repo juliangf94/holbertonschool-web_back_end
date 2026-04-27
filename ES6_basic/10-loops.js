678aafccc4f2da40fe7502726a6bac99ad557181
juliangf94 <juliangf94@hotmail.com>
2026-04-27 15:37:04 +0200

    Fix the task 10

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const [idx, value] of array.entries()) {
-    // eslint-disable-next-line no-param-reassign
     array[idx] = appendString + value;
   }
 
